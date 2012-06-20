9a5e5cf0a9a01679ca3482386af1ae397f0f300e
Justin Martin <frozenfire@thefrozenfire.com>
2012-06-20 10:38:32 -0700

    Spaces to tabs

--- librarygenerator.php
+++ librarygenerator.php
@@ -1,19 +1,19 @@
 <?php
 function is_includeable($filename, $returnpaths = false) {
-    $include_paths = explode(PATH_SEPARATOR, ini_get('include_path'));
+	$include_paths = explode(PATH_SEPARATOR, ini_get('include_path'));
 
-    foreach ($include_paths as $path) {
-        $include = $path.DIRECTORY_SEPARATOR.$filename;
-        if (is_file($include) && is_readable($include)) {
-            if ($returnpaths == true) {
-                $includable_paths[] = $path;
-            } else {
-                return true;
-            }
-        }
-    }
+	foreach ($include_paths as $path) {
+		$include = $path.DIRECTORY_SEPARATOR.$filename;
+		if (is_file($include) && is_readable($include)) {
+			if ($returnpaths == true) {
+				$includable_paths[] = $path;
+			} else {
+				return true;
+			}
+		}
+	}
 
-    return (isset($includeable_paths) && $returnpaths == true) ? $includeable_paths : false;
+	return (isset($includeable_paths) && $returnpaths == true) ? $includeable_paths : false;
 }
 
 function split_file($file, $dest) {
@@ -21,37 +21,37 @@ function split_file($file, $dest) {
 	$buffer = false;
 
 	while ($token = next($tokens)) {
-	    if ($token[0] == T_CLASS) {
+		if ($token[0] == T_CLASS) {
 		$buffer = true;
 		$name = null;
 		$code = '';
 		$braces = 1;
 		do {
-		    $code .= is_string($token) ? $token : $token[1];
-		    if (is_array($token) 
-		        && $token[0] == T_STRING 
-		        && empty($name)) {
-		        $name = $token[1];
-		    }
+			$code .= is_string($token) ? $token : $token[1];
+			if (is_array($token) 
+				&& $token[0] == T_STRING 
+				&& empty($name)) {
+				$name = $token[1];
+			}
 		} while (!(is_string($token) && $token == '{') && $token = next($tokens));
-	    } elseif ($buffer) {
+		} elseif ($buffer) {
 		if (is_string($token)) {
-		    $code .= $token;
-		    if ($token == '{') {
-		        $braces++;
-		    } elseif ($token == '}') {
-		        $braces--;
-		        if ($braces == 0) {
-		            $buffer = false;
-		            $file = $dest . '/' . $name . '.php';
-		            $code = '<?php' . PHP_EOL . $code . PHP_EOL . '?>';
-		            file_put_contents($file, $code); 
-		        }
-		    }
+			$code .= $token;
+			if ($token == '{') {
+				$braces++;
+			} elseif ($token == '}') {
+				$braces--;
+				if ($braces == 0) {
+					$buffer = false;
+					$file = $dest . '/' . $name . '.php';
+					$code = '<?php' . PHP_EOL . $code . PHP_EOL . '?>';
+					file_put_contents($file, $code); 
+				}
+			}
 		} else {
-		    $code .= $token[1];
+			$code .= $token[1];
 		}
-	    }
+		}
 	}
 }
 
@@ -116,9 +116,9 @@ foreach(glob("splitteroutput/*_x0020_*.php") as $serviceFile) {
 	$file = file_get_contents($serviceFile);
 	$constructorcode = <<<EOD
 if(!ini_get('user_agent')) ini_set('user_agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.19) Gecko/20110707 Firefox/3.6.19');
-    \$directory = dirname(__FILE__).DIRECTORY_SEPARATOR;
-    foreach(\$options['classmap'] as \$key => \$value) if(file_exists("{\$directory}../structures/{\$value}.php")) include_once("{\$directory}../structures/{\$value}.php");
-    parent::__construct(\$wsdl, \$options);
+	\$directory = dirname(__FILE__).DIRECTORY_SEPARATOR;
+	foreach(\$options['classmap'] as \$key => \$value) if(file_exists("{\$directory}../structures/{\$value}.php")) include_once("{\$directory}../structures/{\$value}.php");
+	parent::__construct(\$wsdl, \$options);
 EOD;
 	$aliascode = <<<EOD
 class_alias("{$serviceName}", "{$oldServiceName}");
